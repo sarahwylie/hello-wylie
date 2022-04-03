57fe234098288d9590eb033a01b307aff704aa93
sarahwylie <sarahannemariawylie@gmail.com>
2022-04-03 14:18:07 -0500

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import  { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import './index.css'
 import About from './components/About'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import Resume from './components/Resume'
 
 function App() {
-
-  const [contactSelected, setContactSelected] = useState(false);
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   
   return (
     <div>
-      <Nav categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
-      contactSelected={contactSelected}
-      setContactSelected={setContactSelected}></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-            <About></About>
-          </>
-        ) : (
-          <Contact></Contact>
-        )}
-      </main>
+      <Router>
+      <Nav></Nav>
+      <div className="content">
+                <Routes>
+                    <Route path="/" component={About}/>
+                    <Route path="/contact" component={Contact}/>
+                    <Route path="/portfolio" component={Portfolio}/>
+                    <Route path="/resume" component={Resume}/>
+                </Routes>
+            </div>
+        </Router>
       <Footer></Footer>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,7 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../utils/helpers'
+import React from 'react';
 
-function Nav(props) {
-    const {
-        categories=[],
-        setCurrentCategory,
-        currentCategory,
-        contactSelected,
-        setContactSelected
-    } = props;
+function Nav() {
 
-    useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
-    }, [currentCategory]);
 
     return (
         <header>
@@ -30,21 +19,11 @@ function Nav(props) {
                 </div>
                 <nav>
                     <ul className="flex-row">
-                        <li className="mx-2">
-                            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-                                About me
-                            </a>
-                    </li>
-                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-                        <span onClick={() => setContactSelected(true)}>Contact</span>
-                    </li>
-                    {categories.map((category) => (
-                        <li className={`mx-1 ${currentCategory.name === category.name && !contactSelected && 'navActive'}`}
-                        key={category.name}>
-                            <span onClick={() => { setCurrentCategory(category); setContactSelected(false) }}>
-                                {capitalizeFirstLetter(category.name)}</span>
-                        </li>
-                    ))}
+                        {/* <li className="mx-2"> */}
+                        <li><Nav exact to="/">About Me</Nav></li>
+                        <li><Nav to="/portfolio">Contact</Nav></li>
+                        <li><Nav to="/about">Portfolio</Nav></li>
+                        <li><Nav to="/resume">Resume</Nav></li>
                 </ul>
             </nav>
             </div>
