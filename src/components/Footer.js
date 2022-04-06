e127fdf6c16b6c8596b02537f8a954b3e49afad8
sarahwylie <sarahannemariawylie@gmail.com>
2022-04-05 20:31:36 -0500

    updated footer and portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { BsLinkedin, BsGithub, BsFillEnvelopeFill } from 'react-icons/bs'
+import { BsLinkedin, BsGithub, BsFillEnvelopeFill, BsInstagram } from 'react-icons/bs'
 
 function Footer() {
     return (
         <section className='flex-row footer'>
-                    <div className='col-4'>
+                    <div className='col-3'>
                         <a href="https://github.com/sarahwylie"
                             alt="github"
                             target="_blank" 
                             rel="noreferrer"><BsGithub /></a></div>
-                    <div className='col-4'>
+                    <div className='col-3'>
                         <a href="https://www.linkedin.com/in/sarahamwylie/"
                             alt="linkedin"                             
                             target="_blank" 
                             rel="noreferrer"><BsLinkedin /></a></div>
-                    <div className='col-4'>
+                    <div className='col-3'>
                         <a href="mailto:sarahannemariawylie@gmail.com"
                             alt="email" 
                             target="_blank" 
                             rel="noreferrer"><BsFillEnvelopeFill /></a></div>
+                    <div className='col-3'>
+                        <a href="https://www.instagram.com/moolovesdonuts/"
+                            alt="instagram" 
+                            target="_blank" 
+                            rel="noreferrer"><BsInstagram /></a></div>
         </section>
     )
 }
