d1c5e412e94ec240312473c06df88a421f35e287
sarahwylie <sarahannemariawylie@gmail.com>
2022-10-06 09:43:38 -0500

    Updates

---
+++ precache-manifest.8ab9574fe2a29febd9e7a6b63080b7a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f980c364ddf9e8c4aae3",
+    "url": "/hello-wylie/static/css/main.35117bf4.chunk.css"
+  },
+  {
+    "revision": "f980c364ddf9e8c4aae3",
+    "url": "/hello-wylie/static/js/main.0bc15e36.chunk.js"
+  },
+  {
+    "revision": "c760f779ac247e214d1f",
+    "url": "/hello-wylie/static/js/runtime~main.bbea16cb.js"
+  },
+  {
+    "revision": "af205712c4e74a81bf85",
+    "url": "/hello-wylie/static/css/2.0fbd28ac.chunk.css"
+  },
+  {
+    "revision": "af205712c4e74a81bf85",
+    "url": "/hello-wylie/static/js/2.97038a14.chunk.js"
+  },
+  {
+    "revision": "e2aa523fcf964839130f",
+    "url": "/hello-wylie/static/js/3.ec459d4c.chunk.js"
+  },
+  {
+    "revision": "b6454ea1e2440e01d4dffb79778fe97d",
+    "url": "/hello-wylie/static/media/Note.b6454ea1.png"
+  },
+  {
+    "revision": "ab61f9035538e494c45c3fcf7b6115b5",
+    "url": "/hello-wylie/static/media/Talk-Tech.ab61f903.png"
+  },
+  {
+    "revision": "87b708e7ead49ed8300b5a6293801e27",
+    "url": "/hello-wylie/static/media/Rainbosplosion.87b708e7.jpg"
+  },
+  {
+    "revision": "997395995eda0aa026ec13db46f50c06",
+    "url": "/hello-wylie/static/media/BeersBands.99739599.png"
+  },
+  {
+    "revision": "427fb70116eded29416140dec763897a",
+    "url": "/hello-wylie/static/media/Cacheching.427fb701.png"
+  },
+  {
+    "revision": "bb44d557dd4cd31ff1ca88c71587f29f",
+    "url": "/hello-wylie/static/media/Password.bb44d557.png"
+  },
+  {
+    "revision": "75c17ea9f21607de25d2e33abcb1602d",
+    "url": "/hello-wylie/static/media/READ.75c17ea9.jpg"
+  },
+  {
+    "revision": "8688b8567bc42051549b05face95ad12",
+    "url": "/hello-wylie/static/media/hawker.8688b856.png"
+  },
+  {
+    "revision": "ac3f7f40caf437dc854ee5e79bdee0dd",
+    "url": "/hello-wylie/index.html"
+  }
+];
